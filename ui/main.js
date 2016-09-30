999780521aa47c9b04f963ff0abfac746fdf268c
Keshav Ashiya <keshavashiya@outlook.com>
2016-09-30 18:49:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,6 +5,11 @@ console.log('Loaded!');
 //move the image
 
 var img = document.getElementById('madi');
+var marginRight = 0;
+function moveLeft () {
+    marginRight = marginRight + 10;
+    img.style.marginRight = marginRight + 'px';
+}
 img.onclick = function () {
-    img.style.marginLeft = '100px';
+    var interval = setInterval(moveLeft, 100);
 };

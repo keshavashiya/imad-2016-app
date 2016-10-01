dd485dcbc9e5ec2277d0bc768c7b7e2d989cddc8
Keshav Ashiya <keshavashiya@outlook.com>
2016-10-01 16:51:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,9 +19,24 @@ var button = document.getElementById('counter');
 var counter = 0;
 
 button.onclick = function () {
-  //make a request to the counter
-  
+  //create a request to the counter
+  var request = new XMLRequest();
   //capture the responceand store it in variable
+  request.onreadystateChange = function () {
+    if (request.readystate === XMLHTTPRequest.Done) {
+        //Take action
+        if (request.status === 200) {
+            var counter = request.responseText;
+            var span = document.getElementById('count');
+            span.InnerHTML = counter.toString();
+        }
+    }  
+    //Not Done Yet
+  };
+  
+  //make a request
+  request.open('GET', "http://keshavashiya.imad.hasura-app.io/counter", true);
+  request.send(null);
   
   //rander the variable in correct span
   counter = counter + 1;

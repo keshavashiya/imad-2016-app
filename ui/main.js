e5805f73e10dff6fdcf937e1b985fdadd9c4eb1a
Keshav Ashiya <keshavashiya@outlook.com>
2016-10-01 16:32:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,3 +13,17 @@ function moveLeft () {
 img.onclick = function () {
     var interval = setInterval(moveLeft, 10);
 };
+
+//counter code
+var button = document.getElementById('counter');
+var counter = 0;
+button.onclick = function () {
+  //make a request to the counter
+  
+  //capture the responceand store it in variable
+  
+  //rander the variable in correct span
+  counter = counter + 1;
+  var span = document.getElementById('count');
+  span.InnerHTML = counter.toString();
+};

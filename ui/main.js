028b62910a3f84ba088f3a37f4bb318a62f77f87
Keshav Ashiya <keshavashiya@outlook.com>
2016-10-01 16:37:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,6 +17,7 @@ img.onclick = function () {
 //counter code
 var button = document.getElementById('counter');
 var counter = 0;
+
 button.onclick = function () {
   //make a request to the counter
   

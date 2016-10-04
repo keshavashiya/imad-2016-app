d1c640aa93c65b08aca70cb37b1304d57733cddf
Keshav Ashiya <keshavashiya@outlook.com>
2016-10-04 18:37:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,7 @@ console.log('Loaded!');
 
 //move the image
 
-var img = document.getElementById('madi');
+/*var img = document.getElementById('madi');
 var marginRight = 0;
 function moveLeft () {
     marginRight = marginRight + 1;
@@ -12,17 +12,17 @@ function moveLeft () {
 }
 img.onclick = function () {
     var interval = setInterval(moveLeft, 10);
-};
+};*/
 
  //counter code
 var button = document.getElementById('counter');
-var counter = 0;
+//var counter = 0;
 button.onclick = function () {
   //create a request to the counter
-  var request = new XMLRequest();
+  var request = new XMLhttpRequest();
   //capture the responceand store it in variable
   request.onreadystateChange = function () {
-    if (request.readystate === XMLHTTPRequest.Done) {
+    if (request.readystate === XMLhttpRequest.DONE) {
         //Take action
         if (request.status === 200) {
             var counter = request.responseText;
@@ -37,8 +37,8 @@ button.onclick = function () {
   request.open('GET', "http://keshavashiya.imad.hasura-app.io/counter", true);
   request.send(null);
   
- //rander the variable in correct span
+ /*//rander the variable in correct span
   counter = counter + 1;
   var span = document.getElementById('count');
-  span.InnerHTML = counter.toString();
+  span.InnerHTML = counter.toString();*/
 };
